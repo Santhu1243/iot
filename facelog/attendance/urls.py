4949873111882131b348ee5aec6169d9b7d36a4c
Santhu1243 <santhosh.sk1243@gmail.com>
2025-02-28 16:46:30 +0530

    n

--- facelog/attendance/urls.py
+++ facelog/attendance/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import capture_attendance
+from .views import process_attendance  
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('attendance', views.cam_home, name='cam_home'),
-  
+    path('capture/', capture_attendance, name='capture_attendance'),
+    path('process_attendance/', process_attendance, name='process_attendance'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
