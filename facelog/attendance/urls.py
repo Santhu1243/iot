213ae75f2527d7aa0243c22cbd08e41a1dae565b
Ramya123706 <ramyanellikumeri@gmail.com>
2025-02-28 17:53:55 +0530

    new2

--- facelog/attendance/urls.py
+++ facelog/attendance/urls.py
@@ -3,10 +3,18 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path
+from .views import employee_login_view
+
+app_name = "attendance"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('attendance', views.cam_home, name='cam_home'),
-  
+    path('employee-login/', employee_login_view, name='employee_login'),
+
+
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
