0ea6942c8346ca10c55ccaef1afdff159cc3fad7
Ramya123706 <ramyanellikumeri@gmail.com>
2025-03-05 10:30:02 +0530

    5

--- facelog/attendance/views.py
+++ facelog/attendance/views.py
@@ -3,14 +3,11 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import cv2
 import numpy as np
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-
-
-
-
-from django.shortcuts import render
 
 def employee_login_view(request):
     return render(request, 'cam_home.html')
@@ -38,7 +35,35 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Employee, Attendance
 from datetime import datetime, timedelta
 
-@csrf_exempt
+def recognize_and_mark_attendance(frame):
+    # Recognize face
+    known_faces = Employee.objects.exclude(face_encoding="").values('id', 'name', 'face_encoding', 'emp_id', 'designation')
+    known_encodings = [np.array(list(map(float, emp['face_encoding'].split(',')))) for emp in known_faces]
+    known_ids = [emp['id'] for emp in known_faces]
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    face_encodings = face_recognition.face_encodings(rgb_frame)
+
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_encodings, face_encoding, tolerance=0.5)
+        if True in matches:
+            matched_id = known_ids[matches.index(True)]
+            employee = Employee.objects.get(id=matched_id)
+
+            # Check if attendance was already marked today
+            today_start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+            attendance_exists = Attendance.objects.filter(employee=employee, timestamp__gte=today_start).exists()
+
+            if attendance_exists:
+                return "Attendance Already Marked"
+
+            # Mark attendance if not already marked
+            Attendance.objects.create(employee=employee)
+            return f"Attendance marked @ {datetime.now().strftime('%I:%M %p')}"
+
+    return "Face not recognized"
+
+
 def process_attendance(request):
     if request.method == "POST":
         data = json.loads(request.body)
